eb557ada10ca1044fdc04f7302f4ee97eb5e1aa2
JohnMonroy <john.monroy@utec.edu.pe>
2024-09-27 18:27:55 -0500

    Inicio de proyecto

---
+++ src/main/java/com/example/pawjourneyy/PawJourneyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawjourneyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawJourneyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawJourneyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pawjourneyy/PawJourneyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawjourneyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawJourneyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
